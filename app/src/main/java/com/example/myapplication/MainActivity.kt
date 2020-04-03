81c64c5738e568ff42bfe795e8cce8135b01a382
Maryam <mry.dashti3@gmail.com>
2020-04-03 18:59:14 +0430

    update happened

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showMessage("Hello Sami from Maryam for second time:*!")
+        showMessage("How you doing?")
 
     }
 
