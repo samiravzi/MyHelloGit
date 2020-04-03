e93961656ca41ac15a841a071dcd3cdd8f332593
Maryam <mry.dashti3@gmail.com>
2020-04-03 18:35:41 +0430

    second update for message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showMessage("Hello Sami from Maryam:*!")
+        showMessage("Hello Sami from Maryam for second time:*!")
 
     }
 
