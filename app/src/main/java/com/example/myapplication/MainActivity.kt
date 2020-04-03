ada300876311b2e3bf61ff48eec470798e62ee1d
Maryam <mry.dashti3@gmail.com>
2020-04-03 19:14:12 +0430

    updating in new branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,12 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
-        showMessage("How you doing?")
+        showMessage("How you doing??")
 
-=======
-        showMessage("Hiiiiii Maryam!")
->>>>>>> 2e73cb469f79160bf10b53bf35925950e435e63e
     }
 
     private fun showMessage(text: String) {
